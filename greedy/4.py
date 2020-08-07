14e1967e20135a08a90b2e5296145a0512472b0c
D36CHOI <40600306+d36choi@users.noreply.github.com>
2020-08-07 20:03:52 +0900

    Create 4.py
    다시 풀어봐야 함.

---
+++ greedy/4.py
@@ -0,0 +1,19 @@
+from sys import stdin
+
+n, k = list(map(int, stdin.readline().split()))
+
+cnt = 0
+
+while n > k:
+    while n%k!=0:
+        cnt += 1
+        n -= 1
+
+    n = n//k
+    cnt += 1
+
+while n!=1:
+    n -= 1
+    cnt += 1
+
+print(cnt)
