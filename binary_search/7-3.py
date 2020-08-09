cf84509a273ae7a14c9718960e44a6491bfc1bdc
D36CHOI <40600306+d36choi@users.noreply.github.com>
2020-08-09 16:32:55 +0900

    함수화를 안해도 풀수 있다. 모든 조건에서 맞는지는 의문. 최대한 덜잘랐다 result = mid 로 기록

--- binary_search/7-3.py
+++ binary_search/7-3.py
@@ -11,24 +11,6 @@ def cal_sum(cut):
             cnt += (ddeok[i] - cut)
     return cnt
 
-def bin_search(N,item):
-    global merchant
-    start = 0
-    end = N-1
-
-    while start <= end:
-        mid = (start + end) // 2
-        if item == merchant[mid]:
-            print("yes",end= ' ')
-            return
-        elif item > merchant[mid]:
-            start = mid + 1
-        elif item < merchant[mid]:
-            end = mid - 1
-
-    print("no", end=' ')
-    return
-
 N, M = map(int,stdin.readline().split())
 ddeok = list(map(int,stdin.readline().split()))
 ddeok.sort()
